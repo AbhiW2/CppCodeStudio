81e610c83633ddcb5b8d609e1c261e0565770bfa
Abhi Wargad <wargadabhi@gmail.com>
2026-01-12 08:09:29 +0530

    Sum

---
+++ sum_of_num.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    int y = 10;
+    cout<<"Enter 1st Number:";
+    cin>>x;
+    cout<<"Enter 2nd Number:";
+    cin>>y;
+    cout<<"Sum is : "<<x+y;
+}
