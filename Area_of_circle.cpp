ddbb54385e5b5affe1c2013db7a75e198274aac5
Abhi Wargad <wargadabhi@gmail.com>
2026-01-15 21:10:44 +0530

    Removed Three_digit_or_not program

--- Area_of_circle.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
- int main(){
-    float r=8;
-    float area = 3.1415*r*r;
-    cout<<"Area of circle is:";
-    cout<<area;
- }
--- float.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    float x= 5.9; float y = 2.5;
-    cout<<x+y<<endl;
-    cout<<x-y<<endl;
-    cout<<x*y<<endl;
-    cout<<x/y<<endl;
-}
