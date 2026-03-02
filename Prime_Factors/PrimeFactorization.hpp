d85a671639f51e8a2f3f0cd083bb5cfa66ee98d6
Abhi Wargad <wargadabhi@gmail.com>
2026-03-02 15:03:19 +0530

    header file

---
+++ Prime_Factors/PrimeFactorization.hpp
@@ -0,0 +1,10 @@
+#ifndef PRIMEFACTORIZATION_HPP
+#define PRIMEFACTORIZATION_HPP
+
+class PrimeFactorization{
+    public:
+        void findPrimeFactors(int number);
+        
+};
+
+#endif // PRIMEFACTORISATION_HPP
